f261440783e8c385985d15790a9608bc5d9bbb23
Marcos Leva Gomez <marcos@cococo.ventures>
2025-12-26 22:39:01 +0000

    sigue haciendo scroll

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -64,7 +64,7 @@ export default function Sidebar({ isMobile = false }) {
   };
   
   const header = (
-     <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+     <div className="flex h-14 shrink-0 items-center border-b px-4 lg:h-[60px] lg:px-6">
         <Link href="/" className="flex items-center gap-2 font-semibold">
             <ShoppingCart className="h-6 w-6 text-primary" />
             <span className="">Visor de Inventarios</span>
@@ -92,7 +92,7 @@ export default function Sidebar({ isMobile = false }) {
 
   return (
     <div className="hidden border-r bg-card md:block">
-        <div className="flex h-full max-h-screen flex-col">
+        <div className="flex h-full flex-col">
             {header}
             {content}
         </div>
